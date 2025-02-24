ea956afaaaad70d5a8959e98d72144235c48fd9d
gela pipia <gelapipia10@gmail.com>
2025-02-24 09:55:53 +0400

    styled footer for all screen sizes & FT: mobile menu navigation

---
+++ script.js
@@ -0,0 +1,17 @@
+const menu_btn = document.getElementById("menu_btn");
+const menu_icon = document.getElementById("menu_icon");
+const mobile_menu = document.getElementById("mobile_menu");
+
+let isMenuClicked = false;
+
+menu_btn.addEventListener("click", (e) => {
+    if(isMenuClicked) {
+        menu_icon.setAttribute("src", "./assets/shared/mobile/menu.svg");
+        mobile_menu.style.display = "none"
+        isMenuClicked = !isMenuClicked
+    } else {
+        menu_icon.setAttribute("src", "./assets/shared/mobile/close.svg");
+        mobile_menu.style.display = "flex"
+        isMenuClicked = !isMenuClicked
+    }
+})
