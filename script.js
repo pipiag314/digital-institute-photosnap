a1ba06a0de8d68de1f384a5ee661511f4898f3e1
gela pipia <gelapipia10@gmail.com>
2025-02-24 10:09:12 +0400

    fix: dissapear mobile menu when screen resize

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ const mobile_menu = document.getElementById("mobile_menu");
 let isMenuClicked = false;
 
 menu_btn.addEventListener("click", (e) => {
-    if(isMenuClicked) {
-        menu_icon.setAttribute("src", "./assets/shared/mobile/menu.svg");
-        mobile_menu.style.display = "none"
-        isMenuClicked = !isMenuClicked
-    } else {
-        menu_icon.setAttribute("src", "./assets/shared/mobile/close.svg");
-        mobile_menu.style.display = "flex"
-        isMenuClicked = !isMenuClicked
-    }
-})
+  if (isMenuClicked) {
+    menu_icon.setAttribute("src", "./assets/shared/mobile/menu.svg");
+    mobile_menu.style.display = "none";
+    isMenuClicked = !isMenuClicked;
+  } else {
+    menu_icon.setAttribute("src", "./assets/shared/mobile/close.svg");
+    mobile_menu.style.display = "flex";
+    isMenuClicked = !isMenuClicked;
+  }
+});
+
+window.addEventListener("resize", () => {
+  menu_icon.setAttribute("src", "./assets/shared/mobile/menu.svg");
+  mobile_menu.style.display = "none";
+});
